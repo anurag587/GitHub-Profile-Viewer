18de376bb9189b8d9dae430e852cb91d51548408
Anurag Sharma <anuraguict09@gmail.com>
2024-05-04 12:49:31 +0530

    creating search input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Search from "./Components/Search";
-
+import UserInfo from "./Components/UserInfo";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Search />
+    <div className="bg-gradient-to-r from-purple-500 to-pink-500">
+      <Search/>
     </div>
   );
 }
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,36 +1,34 @@
-import { TextInput, Button } from 'flowbite-react';
+import { TextInput, Button } from "flowbite-react";
 
 const Search = () => {
   return (
     <div
       className="min-h-screen flex flex-col items-center justify-center"
-      style={{
-        backgroundImage: "url('github_bg.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
     >
-      <div className="bg-black  p-8 rounded-lg">
+      <div className="bg-gradient-to-r from-cyan-500 to-blue-500 p-8 rounded-lg">
         <div className="flex ">
           {/* GitHub Logo */}
           <div className="flex-shrink-0 mr-4">
-            <img
-              className="h-12"
-              src="github.png"
-              alt="GitHub Logo"
-            />
+            <img className="h-12" src="github.png" alt="GitHub Logo" />
           </div>
           {/* GitHub Profile Viewer */}
           <div className="text-left">
-            <h1 className="text-white text-4xl font-bold mb-4">GitHub Profile Viewer</h1>
-            <p className="text-white mb-4">A Simple Way To Show Your GitHub Profile & Repos</p>
+            <h1 className="text-white text-4xl font-bold mb-4">
+              GitHub Profile Viewer
+            </h1>
+            <p className="text-white mb-4">
+              A Simple Way To Show Your GitHub Profile & Repos
+            </p>
             <div className="w-full flex items-center">
               <TextInput
                 type="text"
                 placeholder="Search GitHub Profile"
-                className="w-full py-2 px-4 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring focus:border-blue-300 bg-gray-800 text-white"
+                className="w-full py-2 px-4 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring focus:border-blue-300 bg-gray-800 text-black"
               />
-              <Button gradientDuoTone="purpleToPink" className="ml-2 px-4 py-2 rounded-md shadow-md text-white bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring focus:border-purple-300">
+              <Button
+                gradientDuoTone="purpleToPink"
+                className="ml-2 px-4 py-2 rounded-md shadow-md text-white bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring focus:border-purple-300"
+              >
                 View
               </Button>
             </div>
