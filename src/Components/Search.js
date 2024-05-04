277538d2948077f8de4725f524da15ed601c5ef2
Anurag Sharma <anuraguict09@gmail.com>
2024-05-04 12:52:43 +0530

    making a state to enter the search input

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,6 +1,8 @@
 import { TextInput, Button } from "flowbite-react";
+import { useState } from "react";
 
 const Search = () => {
+    const[userName,setUserName] = useState('');
   return (
     <div
       className="min-h-screen flex flex-col items-center justify-center"
@@ -23,6 +25,8 @@ const Search = () => {
               <TextInput
                 type="text"
                 placeholder="Search GitHub Profile"
+                value={userName}
+                onChange={(e)=>setUserName(e.target.value)}
                 className="w-full py-2 px-4 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring focus:border-blue-300 bg-gray-800 text-black"
               />
               <Button
