ad21abc7930948154dc3e8cfa942f41512c12171
Anurag Sharma <anuraguict09@gmail.com>
2024-05-20 23:20:49 +0530

    created sort functionality

---
+++ src/Components/RepoCard.js
@@ -0,0 +1,86 @@
+import React from "react";
+import mockData from "../mock.json";
+
+const userRepos = mockData.userReposData;
+
+console.log(userRepos);
+export const RepoCard = ({
+  name,
+  description,
+  language,
+  forks_count,
+  stargazers_count,
+  size,
+}) => {
+  // console.log(mockData.userReposData)
+  return (
+    <div className="mt-5 mb-5">
+      <div className="h-56 w-96 bg-white content-center ml-2 mr-2 p-4 rounded-3xl shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 border-solid border-2 border-neutral-950 ">
+        <span className="flex items-center gap-2 mb-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+            />
+          </svg>
+          <h1 className="text-black ">{name}</h1>
+        </span>
+        <span>
+          <p className="text-gray-600 mb-10">{description}</p>
+        </span>
+        <div className="flex justify-between mt-2">
+          <p className="text-gray-600">{language}</p>
+          <span className="flex flex-row gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.867 19.125h.008v.008h-.008v-.008Z"
+              />
+            </svg>
+            <p className="text-gray-600">{forks_count}</p>
+          </span>
+          <span className="flex flex-row gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+            <p className="text-gray-600">{stargazers_count}</p>
+          </span>
+
+          <p className="text-gray-600">{size} KB</p>
+        </div>
+      </div>
+    </div>
+  );
+};
